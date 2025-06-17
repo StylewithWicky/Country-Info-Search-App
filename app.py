b200e7ff9af1bdecbe6803e72c2d5720d8a87f53
Yazz <itsjustmaitai@gmail.com>
2025-06-17 14:00:11 +0300

    Fixed some bugs

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from config import Config
 app=Flask(__name__)
 app.config.from_object(Config)
 
-@app.route('/' , methods=['GET'])
+@app.route('/' , methods=['GET','POST'])
 
 def index():
     new_data=None
@@ -13,19 +13,20 @@ def index():
 
     if news:
         api_key=app.config['NEW_API_KEY']
-        url=f'hhttps://newsapi.org/v2/everything?q={news}&sortBy=publishedAt&apiKey={api_key}'
+        url=f'https://newsapi.org/v2/everything?q={news}&sortBy=publishedAt&apiKey={api_key}'
         response=requests.get(url)
 
-    if response.status_code==200:
-        data=response.json()
-        articles= data.get('articles', [])
+        if response.status_code==200:
+         data=response.json()
+         articles= data.get('articles', [])
 
         if articles: 
             first=articles[0]  
 
             new_data={
             'author':first.get('author'),
-            'name':first.get('name'),
+            'name': first.get('source', {}).get('name'),
+
             'title':first.get('title'),
             'description':first.get('description')
 
@@ -34,6 +35,8 @@ def index():
             new_data={ 'error':'Could not find articles'}
     else:
         new_data={'error':'Failed to get news'}
-    return render_template('index.html',news=new_data)
+    return render_template('index.html', news=new_data)
 
       
+if __name__=='__main__':
+    app.run(debug=True)
